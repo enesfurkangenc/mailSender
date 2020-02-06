4c949801431f36669573864e4d09be76b77558e6
Enes Enesgenc Aleyna <enesgenc@Eness-MacBook-Pro.local>
2020-02-06 18:14:12 +0300

    callback updated

--- index.js
+++ index.js
@@ -14,8 +14,17 @@ app.post("/mailSender", (req, res) => {
     const { to, from, subject, html } = req.body;
     // NOTE: SendGrid uzerinden generate olmus tokenlar ayda 40.000 mail iletebilmektedir.
     // API KEY kullanım süresi 100 gündür.
-    mailSender.MailProcess(to, from, subject, html);
-    res.send({ message: "Başarılı", status: true });
+    mailSender.MailProcess(to, from, subject, html, function(responseCode) {
+      if (responseCode === 401) {
+        res.statusCode = responseCode;
+        res.send({
+          message: "Hata olustu = kullanıcı token süresi bitmiştir",
+          status: false,
+        });
+      } else {
+        res.send({ message: "Başarılı", status: true });
+      }
+    });
   } catch (error) {
     res.send({ message: `Hata Olustu --> ${error}`, status: false });
   }
--- mail.js
+++ mail.js
@@ -1,9 +1,14 @@
 const sgMail = require("@sendgrid/mail");
 
-const MailProcess = function(to, from, subject, html) {
+const MailProcess = function(to, from, subject, html, ResponseCode) {
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
-  sgMail.send({ to, from, subject, html });
+  sgMail.send({ to, from, subject, html }).then(response => {
+    const { code } = response;
+    ResponseCode(code);
+  }).catch(error => {
+    const { code } = error;
+    ResponseCode(code);
+  });
 };
 
 module.exports = { MailProcess };
